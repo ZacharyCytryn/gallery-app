357a9536c6048fa86a2d2a3e760e0202ab2237c9
Zachary Cytryn <zacharycytryn@gmail.com>
2023-01-11 23:08:12 -0500

    Add files via upload

---
+++ IPhotoImporter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using GalleryApp.Models;
+
+namespace GalleryApp
+{
+    public interface IPhotoImporter
+    {
+        Task<ObservableCollection<Photo>> Get(int start, int count,
+            Quality quality = Quality.Low);
+        Task<ObservableCollection<Photo>> Get(List<string> filenames,
+            Quality quality = Quality.Low);
+    }
+
+    public enum Quality
+    {
+        Low,
+        High
+    }
+}
